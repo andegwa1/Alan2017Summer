2ee63e9ca701efb54baa1d11c6250ddd74f2de13
andegwa1 <andegwa1@student.gsu.edu>
2017-06-14 01:19:46 -0400

    6/14 Cosmetics

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/al/alan2017summer/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.al.alan2017summer.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.al.alan2017summer.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
